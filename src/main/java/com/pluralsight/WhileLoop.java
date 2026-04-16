864aa0ff3d37538177443b6cd59930fc08fbdad2
christian-nieves <thechristiannieves@gmail.com>
2026-04-16 12:31:55 -0400

    Prints countdown and launch

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+
+        int i = 0;
+
+        do { System.out.println("I love Java");
+            i++;
+        } while (i < 5);
+
+    }
+}
